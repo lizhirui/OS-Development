09875384f13e050271cce385fa12084d652cd4b8
lizhirui <exiis@126.com>
2019-09-20 23:46:41 +0800

    Add files via upload

---
+++ os/bochsdebug.bat
@@ -0,0 +1,2 @@
+cd D:\程序\项目\20160301操作系统开发\os
+D:\Tools\Bochs-2.6.8\bochsdbg.exe -q -f os.bxrc
---
+++ os/mk.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+echo.
+echo.
+echo.
+echo -------------------------------------------------------
+echo             	   Completing...              
+echo -------------------------------------------------------
+echo.
+echo.
+echo.
+echo.
+
+..\tools\make.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+pause
